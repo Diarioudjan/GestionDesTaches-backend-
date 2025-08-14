af569d5090c2fb700d9194e3d5905d95e9e5c636
diarioudjanb2021@gmail.com <diarioudjanb2021@gmail.com>
2025-08-14 23:18:49 +0000

    modification du readme

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config(); // Charge les variables d'environnement depuis le fichier .env
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('🗄️  MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
